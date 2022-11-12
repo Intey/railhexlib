609340b1a37bfe89eb5b15d744afd1a797fee431
Intey <ziexe0@gmail.com>
2022-11-12 13:52:34 +0300

    build: this should be in the engine repo

--- promote.bat
+++
@@ -1,4 +0,0 @@
-echo "promote.bat: %CD%"
-echo "promote.bat: copy %1 dll to %2/RailHexLib.dll"
-cp bin/%1/netstandard2.0/RailHexLib.dll %2/RailHexLib.dll
-cp bin/%1/netstandard2.0/RailHexLib.pdb %2/RailHexLib.pdb
