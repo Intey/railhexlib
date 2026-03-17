0cc2b9a76926ab5ef4d571c6c292ff176b7e7bb7
Дмитрий Чурин <you@example.com>
2026-03-17 18:08:26 +0300

    feat: nixify

---
+++ nix/deps.nix
@@ -0,0 +1,10 @@
+# Этот файл генерируется автоматически.
+# Чтобы его создать/обновить, выполните из корня проекта:
+#
+#   nix build .#library.passthru.fetch-deps
+#   ./result library/deps.nix
+#
+# Затем добавьте deps.nix в git:
+#   git add library/deps.nix
+
+{ fetchNuGet }: []
